6f10b2de47a5e01f69948bebeeda8fd4cf10d961
oglog <coinop.logan@gmail.com>
2019-04-06 17:24:39 +0700

    Minor Solidity bug

--- src/Network.elm
+++ src/Network.elm
@@ -28,7 +28,7 @@ factoryAddress : Network -> Address
 factoryAddress network =
     case network of
         Mainnet ->
-            Eth.Utils.unsafeToAddress "0x4B354bC1cEDd5887a3D7206a1e0b1f1882C446E0"
+            Eth.Utils.unsafeToAddress "0x6d540C9f4357FE128c6B3300a12A16B38a5bCB3b"
 
         Kovan ->
-            Eth.Utils.unsafeToAddress "0xbE560578C135322B1415b408B632C7C36A25e572"
+            Eth.Utils.unsafeToAddress "0x84aB93f3189715F1757F2F4356dC97B50960Deb6"
